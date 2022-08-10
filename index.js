37d238821077173d5abf8ff4ec96d89099c10935
cheonjp <65703767+cheonjp@users.noreply.github.com>
2022-08-10 07:30:06 -0600

    creating server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const app = require("express")
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const cors = require('cors')
+
+app.use(cors())
+
+const port = 1000
+
+dotenv.config()
+
+
+// mongoose.connect('mongodb+srv://nmmcjp:gksdud29@cluster0.rughgcz.mongodb.net/?retryWrites=true&w=majority', ()=>{
+//     console.log('MongoDB is connected')
+// })
+mongoose.connect(process.env.MONGODB_URL, ()=>{
+    console.log('MongoDB is connected')
+})
+
+app.listen(port, ()=>{
+    console.log(`Backend is under port ${port}`)
+})
