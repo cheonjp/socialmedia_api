1b8bb737feeec109cda9f746faec53f65d8fce70
cheonjp <65703767+cheonjp@users.noreply.github.com>
2022-08-12 08:31:20 -0600

    registeration validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,24 @@
 const router = require("express").Router()
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const { findOne } = require("../models/User")
 
 router.post("/register", async(req,res)=>{
     try {
+        const existUser = await User.findOne({
+            email:req.body.email,
+        })
+        const existUserName = await User.findOne({
+            username:req.body.username,
+        })
+        if(existUser){
+            return res.status(400).json("The email is already registered")
+        }
+        if(existUserName){
+            return res.status(400).json('The user name is already registered')
+        }
         const salt =10
-        const hashedPassword = bcrypt.hash(req.body.password, salt)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
         const newUser = new User({
             username:req.body.username,
             email:req.body.email,
@@ -37,5 +50,5 @@ router.post("/login", async(req,res)=>{
     }
 })
 
-
+// test
 module.exports = router
