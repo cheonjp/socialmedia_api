b69e7c3859d47618d93b581e10480798d87f9d29
cheonjp <65703767+cheonjp@users.noreply.github.com>
2022-10-07 11:44:58 -0600

    setting uploading img

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const cors = require('cors')
+const multer = require("multer")
+const path = require("path")
 
 const authRouter = require("./routes/auth")
 const userRouter = require("./routes/users")
@@ -18,11 +20,32 @@ mongoose.connect(process.env.MONGODB_URL, ()=>{
     console.log('MongoDB is connected')
 })
 
+app.use("/images", express.static(path.join(__dirname,"public/images")))
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"public/images")
+    },
+    filename: (req,file,cb)=>{
+        // cb(null,req.body.name)
+        // cb(null,req.body.name)
+        cb(null,file.originalname)
+    },
+})
+
+
+const upload = multer({storage})
+app.post("/api/upload", upload.single("file"), (req,res)=>{
+    try {
+        return res.status(200).json("File uploaded successfully.")
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 app.use("/api/auth", authRouter)
 app.use("/api/users", userRouter)
 app.use("/api/posts", postRouter)
-
-
 
 app.listen(port, ()=>{
     console.log(`Backend is under port ${port}`)
